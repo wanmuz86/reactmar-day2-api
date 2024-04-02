76f01078b8715321ff0e2fda964c2bc594655472
Wan Muzaffar <wanmuz86@gmail.com>
2024-04-02 15:10:02 +0800

    Input on child passed to parent (City searched)

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@
 import './App.css'
 import Search from './components/Search'
 import Weather from './components/Weather'
+import {useState} from 'react';
 
 function App() {
+  
+  // Weather will be used in Weather Component
+  // It is modified by Search Component
+  // The nearest common parent is App Component
+  // THe weather state will be in this component
 
+const [weather,setWeather] = useState(null);
 
+const handleSearch = (cityName) => {
+  console.log(cityName);
+  /// WHERE THE API CALL WILL HAPPEN
+  
+}
   return (
     <div className='container'>
-     <Search/>
+     <Search onSearchPressed={handleSearch}/>
      <Weather/>
     </div>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 
-const Search = () => {
+const Search = ({onSearchPressed}) => {
+
+    const [cityName, setCityName] = useState('');
+    const handleCityChange = (e) => setCityName(e.target.value);
+
     return (
         <div className='mt-3'>
             <div className='row'>
                 <div className='col-8'>
-                    <input className='form-control' type="text" placeholder='Enter city name' name='city' />
+                    <input className='form-control' type="text" 
+                    placeholder='Enter city name' name='city' 
+                    value={cityName} onChange={handleCityChange}/>
                 </div>
                 <div className='col-4'>
-                    <button className='btn btn-primary' style={{width:'100%'}}>Search Weather</button>
+                    <button className='btn btn-primary' style={{width:'100%'}} 
+                    onClick={()=>onSearchPressed(cityName)}>Search Weather</button>
                 </div>
         </div>
         </div>
