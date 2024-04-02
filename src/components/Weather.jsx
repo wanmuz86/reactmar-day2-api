56935eb99953beb2fdaadfa0e01638fcd471eb3d
Wan Muzaffar <wanmuz86@gmail.com>
2024-04-02 16:24:10 +0800

    fix the time bug

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+const transformWeather = (date) => {
+    return new Date(date*1000).toLocaleTimeString();
+}
 
 const Weather = ({weatherData}) => {
   return (
@@ -13,7 +16,7 @@ const Weather = ({weatherData}) => {
                 <h3>{weatherData.weather[0].main} - {weatherData.weather[0].description}</h3>
                 <p>{Math.round(weatherData.main.temp -273.15)} &deg;C</p>
                 <p>{weatherData.main.humidity}% - {weatherData.main.pressure} hPa</p>
-                <p>{new Date(weatherData.sys.sunrise).toLocaleTimeString()} - {new Date(weatherData.sys.sunset).toLocaleTimeString()}</p>
+                <p>{transformWeather(weatherData.sys.sunrise)} - {transformWeather(weatherData.sys.sunset)}</p>
             </div>
         </div>
     </div>
